d38ed2d36c0ef22485d24c6d15e0bb58f86a1545
Mariya.Vdovina <mariya.vdovina@gmail.com>
2022-12-28 12:36:57 +0300

    Code cleanup

--- src/main/java/providers/UserProvider.kt
+++ src/main/java/providers/UserProvider.kt
@@ -1,9 +1,7 @@
 package providers
 
 import com.github.javafaker.Faker
-import io.qameta.allure.Step
 import models.User
-import providers.CredentialsProvider.Companion.getRandom
 
 class UserProvider {
 
@@ -15,9 +13,4 @@ class UserProvider {
             return User(CredentialsProvider.getRandom().email, CredentialsProvider.getRandom().password, faker.name().firstName())
         }
     }
-}
-fun main(){
-    var user: User = UserProvider.getRandom()
-    println("Name: ${user.name}, email: ${user.email}, password: ${user.password}")
-
 }
